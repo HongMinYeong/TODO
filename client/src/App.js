4a232afe32069588a461ddef83ea30a4215f4a39
HongMinYeong <minyung1240@daum.net>
2023-10-11 18:19:30 +0900

    feat:todo 리스트 보이기

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,36 @@
+import { useState } from 'react';
+import Todo from './components/Todo';
 function App() {
-  return <div className="App"></div>;
+  const [todoItems, setTodoItems] = useState([
+    {
+      id: 1,
+      title: 'my todo1',
+      done: false,
+    },
+    {
+      id: 2,
+      title: 'my todo2',
+      done: false,
+    },
+    {
+      id: 3,
+      title: 'my todo3',
+      done: true,
+    },
+    {
+      id: 4,
+      title: '배고픔',
+      done: true,
+    },
+  ]);
+  return (
+    <div className="App">
+      {/* todoItems 반복, props 데이터(투두 갹채)를 자식 컴포넌트에게 전달 */}
+      {todoItems.map((item) => (
+        <Todo key={item.id} item={item} />
+      ))}
+    </div>
+  );
 }
 
 export default App;
